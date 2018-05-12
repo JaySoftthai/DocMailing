ea7357e8911fd78dc26defa983ebfad75956ab4f
rowenhaward <adisak.m@softthai.com>
2018-05-12 12:41:03 +0700

    2018.05.12.003:fixed scanner can't close in andoid

--- src/pages/app-data/app-data.ts
+++ src/pages/app-data/app-data.ts
@@ -39,21 +39,11 @@ export class AppDataPage {
   }
   CallScaner() {
 
-    this.barcodeScanner.scan({
-      // disableSuccessBeep: false, // iOS and Android
-      // preferFrontCamera: true, // iOS and Android
-      // showFlipCameraButton: true, // iOS and Android
-      // showTorchButton: true, // iOS and Android
-      // torchOn: true, // Android, launch with the torch switched on (if available)
-      // saveHistory: true, // Android, save scan history (default false)
-      // prompt: "Place a barcode inside the scan area", // Android
-      // resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-      // formats: "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-      // orientation: "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
-      // disableAnimations: true, // iOS
-    }).then(barcodeData => {
+    this.barcodeScanner.scan().then(barcodeData => {
 
-      if (barcodeData.cancelled == true) {
+      this.presentToast('barcodeData cancelled: ' + barcodeData.cancelled);
+      if (1 == 1)//(barcodeData.cancelled == true)
+      {
         // alert(“Was cancelled”);
         this.presentToast('barcodeData cancelled: true');
 
